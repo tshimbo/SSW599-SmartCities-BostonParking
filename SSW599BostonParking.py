7c19f174756a82cd6805a0d393aea8f8ea12b92c
Terry Shim <94722131+tshimbo@users.noreply.github.com>
2023-12-08 03:06:10 -0500

    Update SSW599BostonParking.py
    changed comments for code base

--- SSW599BostonParking.py
+++ SSW599BostonParking.py
@@ -2,34 +2,34 @@ import pandas as pd
 import folium
 from folium.plugins import MarkerCluster
 
-# Read CSV data
+# parse through csv data
 csv_file_path = "Parking_Meters.csv"
 df = pd.read_csv(csv_file_path)
 
-# Drop rows with missing latitude, longitude, or ObjectID values
+# error checking 
 df = df.dropna(subset=['LATITUDE', 'LONGITUDE', 'OBJECTID'])
 
-# Check if there are any remaining missing values
+# error checking
 if df[['LATITUDE', 'LONGITUDE', 'OBJECTID']].isnull().values.any():
     print("Warning: There are still missing values in LATITUDE, LONGITUDE, or OBJECTID columns.")
 
-# Create a folium map centered at the mean coordinates of your data
+# folium map is made using the mean of the csv data
 map_center = [df['LATITUDE'].mean(), df['LONGITUDE'].mean()]
 mymap = folium.Map(location=map_center, zoom_start=12)
 
-# Create a MarkerCluster to group markers
+
 marker_cluster = MarkerCluster().add_to(mymap)
 
-# Add markers for each data point
+# add markers for everey CSV data point
 for index, row in df.iterrows():
     coordinates = [row['LATITUDE'], row['LONGITUDE']]
 
-    # Customize the popup content based on your CSV columns
+    # map pointer data showing specific things
     popup_text = f"Object ID: {row['OBJECTID']}<br>Base Rate: {row['BASE_RATE']}<br>Vendor: {row['VENDOR']}"
 
-    # Create a marker with popup
+    # Crreate marker with the popup data
     marker = folium.Marker(location=coordinates, popup=popup_text)
     marker.add_to(marker_cluster)
 
-# Save the map as an HTML file
-mymap.save("interactive_map_with_popup.html")
+# saveing map as HTML
+mymap.save("Boston_Parking_Map.html")
